e9aa07e434051aa437eed61b78b162644f925d25
nakamurammm <mr.mar2.sor@gmail.com>
2021-02-17 04:29:39 +0000

    [4-4]startadd

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4abe441ec64e6876e1ed2eafbac3edf5e258d80633a734df84eda00808d8c6b35e214b34bdb7db34056d18f9d2be664970519f57f85fb89d1a30d415140b030b'

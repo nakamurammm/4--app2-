c8d5381a44993969d2206aee045e643e35fa1796
nakamurammm <mr.mar2.sor@gmail.com>
2021-02-18 04:38:26 +0000

    [4-6]finish

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,8 +6,11 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
-    @post_image.save
-    redirect_to post_images_path
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
   end
 
   def index
@@ -25,7 +28,7 @@ class PostImagesController < ApplicationController
     redirect_to post_images_path
   end
 
-  private
+private
 
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
